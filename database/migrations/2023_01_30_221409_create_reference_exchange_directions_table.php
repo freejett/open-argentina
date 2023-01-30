4728839fd56a50b968f4619fa67e4ae5946d1681
Denis K <jt87@ya.ru>
2023-01-31 01:26:27 +0300

    Миграции для обменных курсов

---
+++ database/migrations/2023_01_30_221409_create_reference_exchange_directions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reference_exchange_directions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('direction_id')->nullable();
+            $table->char('title', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reference_exchange_directions');
+    }
+};
