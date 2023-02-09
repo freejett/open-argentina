5ce19394eba3a28bed1304e3173409782b2a064d
Denis K <jt87@ya.ru>
2023-02-10 02:37:55 +0300

    Update ReferenceExchangeDirections.php

--- app/Models/References/ReferenceExchangeDirections.php
+++ app/Models/References/ReferenceExchangeDirections.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ReferenceExchangeDirections extends Model
 {
     use HasFactory;
+
+    /**
+     * Возвращает направление обмена в виде 'X -> Y'
+     * @return string
+     */
+    public function getDirectionStringAttribute(): string
+    {
+        $parts = explode('|', $this->title);
+        return trim($parts[0]) .' &rarr; '. trim($parts[1]);
+    }
 }
