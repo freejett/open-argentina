2046ea6c56bb2cf26d256e3e56accba8d7751fcb
Denis K <jt87@ya.ru>
2023-02-27 22:44:08 +0300

    Update StringFunctions.php

--- app/Helpers/StringFunctions.php
+++ app/Helpers/StringFunctions.php
@@ -9,7 +9,7 @@ class StringFunctions
      * @param $str
      * @return string
      */
-    public static function nl2br($str)
+    public static function nl2br($str): string
     {
         return (string) str_replace(array("\r\n", "\r", "\n"), "<br>", $str);
     }
@@ -20,7 +20,8 @@ class StringFunctions
      * @param $str
      * @return array|string|string[]|null
      */
-    public static function cleanAddressStr ($str) {
+    public static function cleanAddressStr ($str): array|string|null
+    {
         $str = preg_replace('/[^\wa-zA-Z0-9.\- ]/ui', '', $str );
         return preg_replace('/[\x{0410}-\x{042F}]+.*[\x{0410}-\x{042F}]+/iu', '', $str);
     }
@@ -30,7 +31,8 @@ class StringFunctions
      * @param $str
      * @return array|string|string[]|null
      */
-    public static function cleanDigitsStr ($str) {
+    public static function cleanDigitsStr ($str): array|string|null
+    {
         return preg_replace('/[^0-9]/ui', '', $str );
     }
 
@@ -39,8 +41,19 @@ class StringFunctions
      * @param $str
      * @return array|string|string[]|null
      */
-    public static function cleanDigitsDotStr ($str) {
+    public static function cleanDigitsDotStr ($str): array|string|null
+    {
         return preg_replace('/[^0-9.]/ui', '', $str );
+    }
+
+    /**
+     * Оставляет в строке только цифры, точку и запятую
+     * @param $str
+     * @return array|string|string[]|null
+     */
+    public static function cleanDigitsDotCommaStr ($str): array|string|null
+    {
+        return preg_replace('/[^0-9.,]/ui', '', $str );
     }
 
     /**
@@ -48,8 +61,9 @@ class StringFunctions
      * @param $str
      * @return array|string|string[]|null
      */
-    public static function cleanDigitsEngStr ($str) {
-        return trim(preg_replace("/[^A-Za-z0-9\/\s.]/", "", $str));
+    public static function cleanDigitsEngStr ($str): array|string|null
+    {
+        return trim(preg_replace("/[^A-Za-z0-9\/\s.]/ui", "", $str));
     }
 
     /**
@@ -57,7 +71,8 @@ class StringFunctions
      * @param $str
      * @return array|string|string[]|null
      */
-    public static function cleanDigitsEngRuStr ($str) {
+    public static function cleanDigitsEngRuStr ($str): array|string|null
+    {
         return trim(preg_replace("/[^A-Za-zА-ЯЁа-яё0-9\/\s=-]/ui", "", $str));
     }
 
@@ -66,8 +81,24 @@ class StringFunctions
      * @param $str
      * @return string
      */
-    public static function clearUmlauts($str)
+    public static function clearUmlauts($str): string
     {
         return preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml|caron);~i', '$1', htmlentities($str, ENT_COMPAT, 'UTF-8'));
+    }
+
+    /**
+     * Заменить emoji на сущности
+     * @param $str
+     * @return array|string|string[]|null
+     */
+    public static function emojiToUnicode($str) {
+        return preg_replace_callback(
+            "%(?:\xF0[\x90-\xBF][\x80-\xBF]{2} | [\xF1-\xF3][\x80-\xBF]{3} | \xF4[\x80-\x8F][\x80-\xBF]{2})%xs",
+            function($emoji){
+                $emojiStr = mb_convert_encoding($emoji[0], 'UTF-32', 'UTF-8');
+                return strtoupper(preg_replace("/^[0]+/","U+",bin2hex($emojiStr)));
+            },
+            $str
+        );
     }
 }
