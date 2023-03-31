ad4ed5565b236cb389d11e141295bfcc31b1ff00
Denis K <jett8786@gmail.com>
2023-03-31 23:52:43 +0300

    Виджет последних новостей

---
+++ resources/views/frontend/_partial/widgets/last_news.blade.php
@@ -0,0 +1,21 @@
+<!--Последние новости -->
+<div class="testimonial-area recent-property" style="background-color: #FCFCFC; padding-bottom: 15px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 col-sm-12 text-center page-title">
+                <!-- /.feature title -->
+                <h2>Свежие новости</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="row testimonial">
+                <div class="col-md-12">
+                    <div id="testimonial-slider">
+                        @widget('recentNews')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/widgets/recent_news.blade.php
@@ -0,0 +1,18 @@
+@foreach($news as $new)
+    <div class="item">
+        <div class="client-text">
+            <p>{!! nl2br($new->announcement) !!}</p>
+            <a target="_blank" href="{{ $tgLinkBase . $new->channel->username .'/'. $new->msg_id }}" class="">Читать полностью &rarr;</a>
+            <br>
+            Канал <a target="_blank" href="{{ $tgLinkBase . $new->channel->username }}">
+                {{ $new->channel->title }}
+            </a><br>
+            <i class="fa fa-calendar-o mr-3"></i>{{ \Illuminate\Support\Carbon::createFromTimestamp($new->date) }}<br>
+        </div>
+        <div class="client-face wow fadeInRight" data-wow-delay=".9s">
+            <a target="_blank" href="{{ $tgLinkBase . $new->channel->username .'/'. $new->msg_id }}">
+                <img alt="{{ $new->channel->title }}" class="img-responsive img-circle" style="width: 50px;" src="{{ asset($avatarPath . $new->channel->chat_id .'/'. $new->channel->chat_photo) }}" />
+            </a>
+        </div>
+    </div>
+@endforeach
