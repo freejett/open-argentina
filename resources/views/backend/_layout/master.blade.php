3bb7132fea4ee0ca5c7848475eced98fb7922745
Denis K <jett8786@gmail.com>
2023-04-02 20:54:26 +0300

    Добавлены теги для ajax запросов

--- resources/views/backend/_layout/master.blade.php
+++ resources/views/backend/_layout/master.blade.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
 
-<html
-    lang="en"
-    class="light-style layout-menu-fixed"
-    dir="ltr"
-    data-theme="theme-default"
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
     data-assets-path="/assets/backend/"
-    data-template="vertical-menu-template-free"
->
+    data-template="vertical-menu-template-free" >
 <head>
 <meta charset="utf-8" />
-<meta
-    name="viewport"
-    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 
 <title>Админка</title>
 
@@ -25,8 +18,7 @@
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
     rel="stylesheet" />
 
 <!-- Icons. Uncomment required icon fonts -->
@@ -107,8 +99,16 @@
 
 <!-- Main JS -->
 <script src="/assets/backend/js/main.js"></script>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 
 <!-- Page JS -->
+@yield('script')
 
 <!-- Place this tag in your head or just before your close body tag. -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
